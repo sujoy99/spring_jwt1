9ee24d78bd7eac45d75bfed202f29584200d5e13
sujoy99 <scg.sujoy@gmail.com>
2021-08-23 19:13:36 +0600

    completed

--- src/main/java/org/itbl/config/WebSecurityConfiguration.java
+++ src/main/java/org/itbl/config/WebSecurityConfiguration.java
@@ -1,6 +1,10 @@
 package org.itbl.config;
 
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 import org.itbl.JwtFilter;
 import org.itbl.service.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +20,9 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 
 @Configuration
@@ -43,6 +50,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 		
 		
 		http
+			.cors().and()
 			.csrf().disable()
 			.authorizeRequests()
 			.antMatchers(HttpMethod.POST, "/authenticate").permitAll()
@@ -55,6 +63,23 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 		
 		
 	}
+	
+	
+	//and cors configuration
+//  @Bean
+//  CorsConfigurationSource corsConfigurationSource() {
+//      CorsConfiguration configuration = new CorsConfiguration();
+//
+//      configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+//      configuration.setAllowedHeaders(List.of("*"));
+//      configuration.setAllowedMethods(Arrays.asList("GET","POST", "PUT", "DELETE"));
+//      configuration.setAllowCredentials(true);
+//
+//      UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//      source.registerCorsConfiguration("/**", configuration);
+//
+//      return source;
+//  }
 
 	@Override
 	@Bean
